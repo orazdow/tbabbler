6a60964b205982eb39fc47546a675abbc2edc511
orazdow <ollierazdow@hotmail.com>
2018-01-03 05:59:07 -0500

    back to params.txt

--- babbler.py
+++ babbler.py
@@ -132,7 +132,7 @@ class MyStreamer(TwythonStreamer):
 
 class ParamsTxt:
 
-	def __init__(self, ppath, apath):
+	def __init__(self, path):
 		self.streamParams = ''
 		self.ignoreParams = []
 		self.ignoreRetweets = False
@@ -140,9 +140,8 @@ class ParamsTxt:
 		self.auth_set = False
 		self.auth = {}
 		self.paramInit = '# put one or more search terms below:  ex: large cat, devito, scones\nhodl\n\n# put ignore terms below:  ex: offer, sale, newsletter\noffer, sale, newsletter\n\n# uncomment (#) to ignore retweets:\n#ignore-retweets'
-		self.authinit = '#twitter authentication goes here:\nAPP_KEY = "..."\nAPP_SECRET = "..."\nOAUTH_TOKEN = "..." \nOAUTH_TOKEN_SECRET = "..."'
-		self.get_params(ppath)
-		self.get_auth(apath)
+		self.authinit = '\n\n#twitter authentication goes here:\nAPP_KEY = "..."\nAPP_SECRET = "..."\nOAUTH_TOKEN = "..." \nOAUTH_TOKEN_SECRET = "..."'
+		self.get_params(path)
 
 	def get_params(self, path):
 
@@ -161,47 +160,33 @@ class ParamsTxt:
 					elif not self.is_quote(line):
 						if line.find('ignore-retweets') > -1:
 							self.ignoreRetweets = True
+						elif line.find('APP_KEY') > -1:
+							a = line[line.find('=')+1:].replace(' ', '').replace('"', '').replace('\'', '').rstrip()
+							if a != '...':
+								self.auth['APP_KEY'] = a
 
+						elif line.find('APP_SECRET') > -1:
+							a = line[line.find('=')+1:].replace(' ', '').replace('"', '').replace('\'', '').rstrip()
+							if a != '...':
+								self.auth['APP_SECRET'] = a
 
-	def get_auth(self, path):
-		if not os.path.isfile(path):
-			print('could not find authentication.txt')
-			f = open(path, 'w', encoding='utf-8')
-			f.write(self.authinit)
-			f.close()
-		else:
-			with open(path, 'r', encoding='utf-8') as f:
-				for line in f:
-					if not len(line.strip()) == 0:
-						if not self.is_quote(line):
-							if line.find('APP_KEY') > -1:
-								a = line[line.find('=')+1:].replace(' ', '').replace('"', '').replace('\'', '').rstrip()
-								if a != '...':
-									self.auth['APP_KEY'] = a
+						elif line.find('OAUTH_TOKEN') > -1 and line.find('OAUTH_TOKEN_SECRET') == -1:
+							a = line[line.find('=')+1:].replace(' ', '').replace('"', '').replace('\'', '').rstrip()
+							if a != '...':							
+								self.auth['OAUTH_TOKEN'] = a
 
-							elif line.find('APP_SECRET') > -1:
-								a = line[line.find('=')+1:].replace(' ', '').replace('"', '').replace('\'', '').rstrip()
-								if a != '...':
-									self.auth['APP_SECRET'] = a
-
-							elif line.find('OAUTH_TOKEN') > -1 and line.find('OAUTH_TOKEN_SECRET') == -1:
-								a = line[line.find('=')+1:].replace(' ', '').replace('"', '').replace('\'', '').rstrip()
-								if a != '...':							
-									self.auth['OAUTH_TOKEN'] = a
-
-							elif line.find('OAUTH_TOKEN_SECRET') > -1:
-								a = line[line.find('=')+1:].replace(' ', '').replace('"', '').replace('\'', '').rstrip()
-								if a != '...':
-									self.auth['OAUTH_TOKEN_SECRET'] = a
+						elif line.find('OAUTH_TOKEN_SECRET') > -1:
+							a = line[line.find('=')+1:].replace(' ', '').replace('"', '').replace('\'', '').rstrip()
+							if a != '...':
+								self.auth['OAUTH_TOKEN_SECRET'] = a
 
 		self.auth_set = len(self.auth) == 4
-
 
 	def init_params(self, path):
 		if not os.path.isfile(path):
 			f = open(path, 'w', encoding='utf-8')
 			f.write(self.paramInit)
-			# f.write(self.authinit)
+			f.write(self.authinit)
 			f.close()		
 
 	def get_stream_params(self):
@@ -230,10 +215,10 @@ class ParamsTxt:
 
 
 def main():
-	p = ParamsTxt('params.txt', 'auth.txt')
+	p = ParamsTxt('params.txt')
 
 	if(not p.auth_set):
-		print('authentication not found\nexiting...')
+		print('authentication not found in params.txt\nexiting...')
 		input()
 		return
 
